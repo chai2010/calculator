05d3405709a0743e1187b63fcb16c2e905ca7e36
chai2010 <chaishushan@gmail.com>
2019-03-02 10:31:36 +0800

    完善说明文件

--- sym.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2019 <chaishushan{AT}gmail.com>. All rights reserved.
-// Use of this source code is governed by a Apache
-// license that can be found in the LICENSE file.
-
-package main
